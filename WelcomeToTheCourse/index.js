0e607022b5956914b2c8dc4499a519dadb0b8625
chrismclellan1986 <cmclellan@live.co.uk>
2021-12-27 13:49:17 +0000

    Code up to Section 1 Lession 26 1 hour 5 min mark

--- WelcomeToTheCourse/index.js
+++ WelcomeToTheCourse/index.js
@@ -1,17 +1,49 @@
-// document.getElementById("count-el").innerText = 5
-
-//let count = 5;
-//count =  count + 3;
-//console.log(count)
-
-//intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clocked
-// chnage the count-el in the HTML to reflect the new count
-
+let countEL = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0;
 
 function increment() {
     count += 1
-    document.getElementById("count-el").innerText = count
+    countEL.innerText = count
 }
+
+function save() {
+    let previousCountStr = count + " - "
+    saveEl.innerText += previousCountStr
+    console.log(count)
+}
+
+
+//
+
+
+let username = "Chris";
+let message = "You have three new notifications"
+
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser);
+
+let name = "Chris";
+let greeting = "Hi, my name is ";
+
+let myGreeting = greeting + name;
+console.log(myGreeting)
+
+//
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+//let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+// let name2 = "Chris"
+// let greeting2 = "Welcome back "
+// let totalGreeting = greeting2 + name2
+
+// // Rendar the welcome message using welcomeEl.innerText
+
+// welcomeEl.innerText = totalGreeting;
+
