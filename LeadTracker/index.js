0fe629be5ee16c4a998c1e1e20f0ea0c65c78a95
chrismclellan1986 <cmclellan@live.co.uk>
2022-01-03 21:27:34 +0000

    All code up to Write Your first innerHTML 5.13

--- LeadTracker/index.js
+++ LeadTracker/index.js
@@ -1,11 +1,15 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
-let myLeads = [];
+const ulEl = document.getElementById("ul-id");
+let myLeads = ["www.mylead1.com","www.myleads2.com","www.myleads3.com"];
 
 inputBtn.addEventListener("click", function() {
-    //console.log("Button clicked")
-    console.log("Before click the myLeads is: " + myLeads)
-    myLeads.push("www.awesomelead.com")
-    console.log("After click the myLeads is: " + myLeads)
-
+    console.log("Before push")
+    myLeads.push(inputEl.value);
+    console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    //console.log(myLeads[i])
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
