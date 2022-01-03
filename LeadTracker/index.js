4f0ec665c7e622befab3e4c4a57641aeafa1caf4
chrismclellan1986 <cmclellan@live.co.uk>
2022-01-03 12:26:52 +0000

    All code upto Section 5 Lession 11 5.01

---
+++ LeadTracker/index.js
@@ -0,0 +1,11 @@
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+let myLeads = [];
+
+inputBtn.addEventListener("click", function() {
+    //console.log("Button clicked")
+    console.log("Before click the myLeads is: " + myLeads)
+    myLeads.push("www.awesomelead.com")
+    console.log("After click the myLeads is: " + myLeads)
+
+})
