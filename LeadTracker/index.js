50d95b49d67a5353d08c1ad8e39767d87daf8d55
Christopher McLellan <christopher.mclellan@bbc.co.uk>
2022-01-05 13:24:30 +0000

    All code update Section 5 Lesson 37 6.13

--- LeadTracker/index.js
+++ LeadTracker/index.js
@@ -1,12 +1,30 @@
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-id");
+
 let myLeads = [];
+
+//myLeads = JSON.parse(myLeads);
+//myLeads.push("www.myleads2.com");
+//myLeads = JSON.stringify(myLeads)
+//console.log(typeof myLeads)
+
+//localStorage.setItem("myLeads1", "www.example1.com")
+//console.log(localStorage.getItem("myLeads1"))
+//localStorage.clear()
+
+const leadsFromLocaStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocaStorage)
+
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
+
+  console.log(localStorage.getItem("myLeads"))
+
 });
 
 function renderLeads() {
